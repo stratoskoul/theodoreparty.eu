c57c0eae59a710be75f13ce4f8f1c4a0a435b6fa
Koulizakis Efstratios <ekoulizakis@etasa.gr>
2025-03-16 17:40:43 +0200

    Last Try

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// Basic JavaScript file - currently empty as the invitation doesn't need interactivity
-// You could add features like an RSVP button or animation here in the future
-
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Spiderman Birthday Invitation Loaded!");
-  // Add any future interactive features here
-});
